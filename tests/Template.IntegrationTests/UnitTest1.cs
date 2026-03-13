317b95cbb1c2c683d824fa31252ddf28f1edb08b
DinisSimoes <di.f.simoes@gmail.com>
2026-03-12 22:37:18 -0300

    Initial clean architecture template

---
+++ tests/Template.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Template.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Template.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Template.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
